cbe257f9565074900cf9c6b7aaa4030a334c5604
sujiayizu <suxinying@outlook.com>
2021-02-01 23:41:30 +0800

    expo init && add react-navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Routes} from "./src/Routes";
+import Examples from "./src/Examples";
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from "@react-navigation/native";
 
+const Stack = createStackNavigator<Routes>();
+const AppNavigator = () => (
+    <Stack.Navigator>
+      <Stack.Screen
+          name="Examples"
+          component={Examples}
+          options={{
+            title: "Reanimated2 Examples"
+          }}
+      />
+    </Stack.Navigator>
+)
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <AppNavigator />
+      </NavigationContainer>
   );
 }
 
---
+++ src/Examples/index.tsx
@@ -0,0 +1,13 @@
+import {ScrollView, Text} from "react-native";
+import React from "react";
+
+
+const Examples = () => {
+    return (
+        <ScrollView>
+            <Text>???</Text>
+        </ScrollView>
+    )
+}
+
+export default Examples;
