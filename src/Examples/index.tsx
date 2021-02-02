4bb935a753a43aca0ad2d0cbefea79b4451fe735
sujiayizu <sujiayizu@outlook.com>
2021-02-02 18:05:58 +0800

    xx

--- src/Examples/index.tsx
+++ src/Examples/index.tsx
@@ -1,11 +1,22 @@
 import {ScrollView, Text} from "react-native";
 import React from "react";
-
+import Animated, {useAnimatedStyle, useSharedValue} from "react-native-reanimated";
 
 const Examples = () => {
+    const size = useSharedValue(true)
+    const style = useAnimatedStyle(() => {
+        return {
+            backgroundColor: size.value ? 'red' : 'yellow'
+        }
+    })
     return (
         <ScrollView>
-            <Text>???</Text>
+            <Animated.View style={style} onPress={() => {
+                alert('?x')
+                size.value = false
+            }}>
+                <Text>???</Text>
+            </Animated.View>
         </ScrollView>
     )
 }
